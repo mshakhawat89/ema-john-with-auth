f5e8fd89b5c5677068b2c7546f311ff0c9b151c3
md shakhawat hossain <hossainmdshakhawat19@gmail.com>
2021-04-06 00:03:37 +0600

    creating ema-john-with-auth client site

---
+++ src/firebaseConfig.js
@@ -0,0 +1,19 @@
+// const firebaseConfig = {
+//     apiKey: "AIzaSyDrnTlaFOThHGej0ZkIglLffFZZioNSPiI",
+//     authDomain: "ema-test-jkr.firebaseapp.com",
+//     databaseURL: "https://ema-test-jkr.firebaseio.com",
+//     projectId: "ema-test-jkr",
+//     storageBucket: "ema-test-jkr.appspot.com",
+//     messagingSenderId: "273630571694",
+//     appId: "1:273630571694:web:dd5435e9e8b7deb372913f"
+//   };
+// export default firebaseConfig;
+const firebaseConfig = {
+  apiKey: "AIzaSyC5D-9C3rvDSD3njxfDreR8RWoPMcJOh80",
+  authDomain: "ema-john-with-auth-25b23.firebaseapp.com",
+  projectId: "ema-john-with-auth-25b23",
+  storageBucket: "ema-john-with-auth-25b23.appspot.com",
+  messagingSenderId: "1092982271970",
+  appId: "1:1092982271970:web:99eb3dba59370b0efc13bb"
+};
+export default firebaseConfig;
